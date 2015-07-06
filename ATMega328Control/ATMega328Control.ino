8d64d386b6a1aec4726b331cf9490939225cd158
shimozuru kota <kota.19900604@gmail.com>
2015-07-06 16:41:52 +0900

    初期ファイル作成

---
+++ ATMega328Control/ATMega328Control.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		ATMega328Control.ino
+ Created:	2015/07/06 16:38:42
+ Author:	John
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
