2c0b38da3781807b9d4392b195fbff38197c6b36
shimozuru kota <kota.19900604@gmail.com>
2015-07-06 18:23:57 +0900

    ピンアサインの設定を完了

--- ATMega328Control/ATMega328Control.ino
+++ ATMega328Control/ATMega328Control.ino
@@ -11,10 +11,11 @@ const int ICSP_MOSI = 11;
 const int ICSP_MISO = 12;
 const int ICSP_SCK = 13;
 Demux demux = Demux(17, 18, 19);
+const int readPin = 7;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-	pinMode(BLEModule_TX, OUTPUT);
+	pinMode(BLEModule_TX, INPUT);
 	pinMode(BLEModule_RX, OUTPUT);
 	pinMode(ICSP_MOSI, OUTPUT);
 	pinMode(ICSP_MISO, OUTPUT);
